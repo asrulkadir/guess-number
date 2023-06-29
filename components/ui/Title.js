152456149948ec567b1d9b945831eed4fc1cd79e
Asrul Kadir <asrulkadir.asus4@gmail.com>
2023-06-29 22:05:34 +0800

    adaptive design

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -17,5 +17,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: '#ffffff',
     padding: 12,
+    maxWidth: '80%',
+    width: 300,
   },
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,4 +1,10 @@
-import { StyleSheet, View, TextInput, Alert } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Alert,
+  useWindowDimensions,
+} from 'react-native';
 import React, { useState } from 'react';
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Colors from '../constants/colors';
@@ -8,6 +14,8 @@ import InstructionText from '../components/ui/InstructionText';
 
 const StartGameScreen = ({ onPickNumber }) => {
   const [enteredNumber, setEnteredNumber] = useState('');
+
+  const { width, height } = useWindowDimensions();
 
   const numberInputHandler = (enteredText) => {
     setEnteredNumber(enteredText);
@@ -30,8 +38,17 @@ const StartGameScreen = ({ onPickNumber }) => {
     onPickNumber(chosenNumber);
   };
 
+  const marginTopDistance = height < 380 ? 30 : 100;
+
   return (
-    <View style={styles.rootContainer}>
+    <View
+      style={[
+        styles.rootContainer,
+        {
+          marginTop: marginTopDistance,
+        },
+      ]}
+    >
       <Title>Guess My Number</Title>
       <Card>
         <InstructionText>Enter a Number</InstructionText>
@@ -62,7 +79,7 @@ export default StartGameScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    marginTop: 100,
+    // marginTop: deviceHeight < 380 ? 30 : 100,
     alignItems: 'center',
   },
   numberInput: {
